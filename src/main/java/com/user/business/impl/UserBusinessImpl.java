574d688ca46de3209b2e6beb8a37e694cae0e9e9
zhuguangquan <zhuguangquan@enmonster.com>
2020-11-10 18:46:49 +0800

    用户系统搭建

--- src/main/java/com/user/business/impl/UserBusinessImpl.java
+++ src/main/java/com/user/business/impl/UserBusinessImpl.java
@@ -7,6 +7,7 @@ import com.user.dto.request.ModifyUserRequestDTO;
 import com.user.dto.response.UserInfoResponseDTO;
 import com.user.service.UserService;
 import org.springframework.stereotype.Component;
+import org.springframework.util.ObjectUtils;
 
 
 import javax.annotation.Resource;
@@ -14,9 +15,6 @@ import javax.annotation.Resource;
 
 @Component
 public class UserBusinessImpl implements UserBusiness {
-//
-//	@Autowired
-//	private RedisTemplate<String, Object> template;
 
 	@Resource
 	private UserService userService;
@@ -50,32 +48,17 @@ public class UserBusinessImpl implements UserBusiness {
 
 	@Override
 	public UserInfoResponseDTO getById(Long id) {
-		//先查看redis缓存是否有用户信息
-//		String userId = String.valueOf(id);
-//		String cacheKey = "ZGQ:" + userId;
-//
-//		ValueOperations<String, Object> redisString = template.opsForValue();
-//		String userJson = (String) redisString.get("cacheKey");
-
-//		if (StringUtils.isEmpty(userJson)) {
 			User user = userService.getById(id);
-//			if (ObjectUtils.isEmpty(user)) {
-//				throw new RuntimeException("用户不存在");
-//			}
+			if(ObjectUtils.isEmpty(user)){
+				throw new RuntimeException("用户不存在");
+			}
 			UserInfoResponseDTO userInfoResponseDTO = new UserInfoResponseDTO();
 			userInfoResponseDTO.setId(user.getId());
 			userInfoResponseDTO.setAge(user.getAge());
 			userInfoResponseDTO.setName(user.getName());
 			userInfoResponseDTO.setSchool(user.getSchool());
 			userInfoResponseDTO.setSex(user.getSex());
-
-//			redisString.set(cacheKey, user, 60L);
-
 			return userInfoResponseDTO;
-//		}
-
-//		return null;
-
 	}
 
 
