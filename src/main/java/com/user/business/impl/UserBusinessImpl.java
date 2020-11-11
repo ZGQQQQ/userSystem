5cdd50cce35f19e375382eea5357c2cbfc21e002
zhuguangquan <zhuguangquan@enmonster.com>
2020-11-11 14:24:01 +0800

    查询用户信息 先查redis再查数据库

--- src/main/java/com/user/business/impl/UserBusinessImpl.java
+++ src/main/java/com/user/business/impl/UserBusinessImpl.java
@@ -6,15 +6,22 @@ import com.user.dto.request.CreateUserRequestDTO;
 import com.user.dto.request.ModifyUserRequestDTO;
 import com.user.dto.response.UserInfoResponseDTO;
 import com.user.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Component;
 import org.springframework.util.ObjectUtils;
-
+import org.springframework.util.StringUtils;
 
 import javax.annotation.Resource;
+import java.util.concurrent.TimeUnit;
 
 
 @Component
 public class UserBusinessImpl implements UserBusiness {
+
+	@Autowired
+	private RedisTemplate<String, Object> template;
 
 	@Resource
 	private UserService userService;
@@ -48,18 +55,35 @@ public class UserBusinessImpl implements UserBusiness {
 
 	@Override
 	public UserInfoResponseDTO getById(Long id) {
-			User user = userService.getById(id);
-			if(ObjectUtils.isEmpty(user)){
-				throw new RuntimeException("用户不存在");
-			}
-			UserInfoResponseDTO userInfoResponseDTO = new UserInfoResponseDTO();
-			userInfoResponseDTO.setId(user.getId());
-			userInfoResponseDTO.setAge(user.getAge());
-			userInfoResponseDTO.setName(user.getName());
-			userInfoResponseDTO.setSchool(user.getSchool());
-			userInfoResponseDTO.setSex(user.getSex());
-			return userInfoResponseDTO;
-	}
+		UserInfoResponseDTO userInfoResponseDTO = new UserInfoResponseDTO();
 
+		//组装key
+		String userId = String.valueOf(id);
+		String cacheKey = "ZGQ:" + userId;
+
+		//先查询redis缓存中是否有用户信息
+		User userJson = (User)template.opsForValue().get(cacheKey);
+		if (!ObjectUtils.isEmpty(userJson)) {
+			userInfoResponseDTO.setId(userJson.getId());
+			userInfoResponseDTO.setName(userJson.getName());
+			userInfoResponseDTO.setAge(userJson.getAge());
+			userInfoResponseDTO.setSex(userJson.getSex());
+			userInfoResponseDTO.setSchool(userJson.getSchool());
+			return userInfoResponseDTO;
+		}
+
+		//若redis中无数据，再去查询数据库
+		User user = userService.getById(id);
+		if (ObjectUtils.isEmpty(user)) {
+			throw new RuntimeException("用户不存在");
+		}
+		userInfoResponseDTO.setId(user.getId());
+		userInfoResponseDTO.setAge(user.getAge());
+		userInfoResponseDTO.setName(user.getName());
+		userInfoResponseDTO.setSchool(user.getSchool());
+		userInfoResponseDTO.setSex(user.getSex());
+		template.opsForValue().set(cacheKey,user);
+		return userInfoResponseDTO;
+	}
 
 }
